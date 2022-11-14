09fc5da834f5ccef1a8fd38d288c3f6e87d594a9
riya-JAINN <riyajainkhh@gmail.com>
2022-11-14 11:12:36 +0530

    initial commit

--- app.js
+++ app.js
@@ -2,18 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const _ = require("lodash");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-const days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
 mongoose.connect("mongodb://localhost:27017/todo");
 const itemsSchema = {
   name: {
@@ -21,56 +13,78 @@ const itemsSchema = {
     required: true,
   },
 };
-
-// app.post("/", (req, res) => {
-//   const item = new Item({ name: req.body.item });
-//   item.save();
-//   res.redirect("/");
-// });
-// app.get("/about", (req, res) => {
-//   res.render("about");
-// });
-app.get("/:name", (req, res) => {
-  const listName = req.params.name;
-  const newSchema = {
-    name: {
-      type: String,
-      required: true,
-    },
-  };
-  const newList = mongoose.model(listName, newSchema);
-  let day = new Date().getDay();
-  day = days[day];
-
-  newList.find({}, (err, data) => {
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+};
+const List = mongoose.model("List", listSchema);
+const Item = mongoose.model("item", itemsSchema);
+app.post("/", (req, res) => {
+  const listName = req.body.List;
+  if (listName == "Today") {
+    const item = new Item({ name: req.body.item });
+    item.save();
+    res.redirect("/");
+  } else {
+    List.findOne({ name: listName }, (error, docs) => {
+      docs.items.push({ name: req.body.item });
+      docs.save();
+      res.redirect(`/${listName}`);
+    });
+  }
+});
+app.get("/", (req, res) => {
+  Item.find({}, (err, data) => {
     if (err) console.log(err);
     else {
-      if (data.length === 0) {
-        newList.insertMany(
+      if (data.length < 1) {
+        Item.insertMany(
           [{ name: "eat food" }, { name: "drink  water" }],
-          (err) => {
+          (err, doc) => {
             if (err) console.log("error");
-            else
-              newList.find({}, (newData) => {
-                data = newData;
-              });
+            else res.render("main", { listName: "Today", newItem: doc });
           }
         );
-      }
-      res.render("main", { day: day, newItem: data });
+      } else res.render("main", { listName: "Today", newItem: data });
     }
   });
 });
-// app.post("/delete/:item", (req, res) => {
-//   const id = req.params.item;
-//   console.log(id);
-//   .deleteOne({ id: id }, (err) => {
-//     if (err) console.log(err);
-//     else res.redirect("/");
-//   });
-// });
-
-app.post("/:name", (req, res) => {});
+app.get("/:name", (req, res) => {
+  const name = _.capitalize(req.params.name);
+  List.findOne({ name: name }, (err, docs) => {
+    if (err) console.log("not found");
+    else {
+      if (docs) {
+        res.render("main", { listName: name, newItem: docs.items });
+      } else {
+        const list = new List({
+          name: name,
+          items: [{ name: "eat food" }, { name: "drink water" }],
+        });
+        list.save();
+        res.render("main", { listName: name, newItem: list.items });
+      }
+    }
+  });
+});
+app.post("/delete/:id", (req, res) => {
+  const listName = req.body.listName;
+  const id = req.params.id;
+  if (listName == "Today") {
+    Item.deleteOne({ _id: id }, (err) => {
+      if (err) console.log(err);
+      else res.redirect("/");
+    });
+  } else {
+    List.findOneAndUpdate(
+      { name: listName },
+      { $pull: { items: { _id: id } } },
+      (error) => {
+        if (!error) res.redirect(`/${listName}`);
+      }
+    );
+  }
+});
 app.listen(3000, () => {
   console.log("server is up and running");
 });
